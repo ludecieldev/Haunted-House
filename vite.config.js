6669fca64d5ad4692b9af456249a288c9d8c6828
Alexandre Garbé <alexandre.garbe@epitech.eu>
2024-08-25 22:20:53 +0200

    feat: pages

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,19 @@
 import restart from 'vite-plugin-restart'
 
 export default {
-    root: 'src/', // Sources files (typically where index.html is)
-    publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
-    server:
-    {
-        host: true, // Open to local network and display URL
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+    root: 'src/', // Dossier des fichiers sources (généralement où se trouve index.html)
+    publicDir: '../static/', // Chemin depuis "root" vers les assets statiques (fichiers servis tels quels)
+    base: '/Haunted-House/', // Chemin de base pour toutes les ressources (IMPORTANT pour GitHub Pages)
+    server: {
+        host: true, // Ouvrir sur le réseau local et afficher l'URL
+        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Ouvrir si ce n'est pas dans un CodeSandbox
     },
-    build:
-    {
-        outDir: '../dist', // Output in the dist/ folder
-        emptyOutDir: true, // Empty the folder first
-        sourcemap: true // Add sourcemap
+    build: {
+        outDir: '../dist', // Sortie dans le dossier dist/
+        emptyOutDir: true, // Vider d'abord le dossier
+        sourcemap: true // Ajouter une sourcemap
     },
-    plugins:
-    [
-        restart({ restart: [ '../static/**', ] }) // Restart server on static file change
+    plugins: [
+        restart({ restart: ['../static/**'] }) // Redémarrer le serveur lors de changements de fichiers statiques
     ],
 }
